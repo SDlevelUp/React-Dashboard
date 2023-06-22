ae88c97b77390a5f22c53aa5feb9a88360fc143c
Sarah Berri <sarahberri@outlook.fr>
2023-06-22 03:06:21 +0200

    debut chart

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,10 +1,59 @@
 import "./chart.scss";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
+
+const data = [
+    {
+        name: 'Djarin',
+        total: 1000,
+    },
+    {
+        name: 'Yanis',
+        total: 1500,
+    },
+    {
+        name: 'Maurice',
+        total: 3450,
+    },
+    {
+        name: 'Ahmed',
+        total: 1000,
+    },
+    {
+        name: 'Pascale',
+        total: 2800,
+    },
+    {
+        name: 'Fred',
+        total: 1500,
+    },
+];
 const Chart = () => {
+
     return (
         <div className="chart">
-            CHART
-        </div>
+            <div>Revenus (Sur les 6 derniers mois)</div>
+            <ResponsiveContainer width="100%" aspect={2 / 1}>
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="total" stroke="green" activeDot={{ r: 8 }} />
+                </LineChart>
+            </ResponsiveContainer>
+        </div >
 
     )
 }
