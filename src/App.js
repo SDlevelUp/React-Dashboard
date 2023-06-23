ab451913c7afdcbdbe857c1d4c904663eb946c36
Sarah Berri <sarahberri@outlook.fr>
2023-06-23 19:00:30 +0200

    ghj

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import List from "./pages/list/List";
-import Single from "./pages/single/Single";
-import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import {  DarkContext } from './Context/DarkContext';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import List from './pages/list/List';
+import Single from './pages/single/Single';
+import New from './pages/new/New';
+
+// Components
+import Sidebar from './components/sidebar/Sidebar';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
     return (
-
         <BrowserRouter>
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route path="login" element={<Login />} />
-                <Route path="users" >
-                    <Route index element={<List />} />
-                    <Route path=":userId" element={<Single />} />
-                    <Route path=":new" element={<New />} />
-                </Route>
-                <Route path="products">
-                    <Route index element={<List />} />
-                    <Route path=":productId" element={<Single />} />
-                    <Route path=":new" element={<New />} />
-                </Route>
-            </Routes>
+            <div className="App">
+                <Sidebar />
+                <div className="App-content">
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="login" element={<Login />} />
+                        <Route path="users">
+                            <Route index element={<List />} />
+                            <Route path=":userId" element={<Single />} />
+                            <Route path=":new" element={<New />} />
+                        </Route>
+                        <Route path="products">
+                            <Route index element={<List />} />
+                            <Route path=":productId" element={<Single />} />
+                            <Route path=":new" element={<New />} />
+                        </Route>
+                    </Routes>
+                </div>
+            </div>
         </BrowserRouter>
     );
 }
